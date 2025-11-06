2f25529132849ad936d4930d4dfabdc686b7cc2a
Martintangen <martin.t.schmidt@hiof.no>
2025-11-06 13:24:05 +0100

    Gjort endringer for å forbedre SEO

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function mobilMenu(){
+			var x = document.getElementById("ham");
+
+			if(x.style.display === "block"){
+				x.style.display = "none";
+			}
+			
+			else {
+				x.style.display= "block";
+			}
+		}
